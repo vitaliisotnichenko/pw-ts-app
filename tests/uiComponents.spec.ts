ae2aec19a6698ebeacf93bc4f907a90603ecd263
Vitalii Sotnichenko <vitalii.sotnichenko@gmail.com>
2025-11-22 16:55:52 +0100

    add tests for components

---
+++ tests/uiComponents.spec.ts
@@ -0,0 +1,200 @@
+import {test, expect} from '@playwright/test'
+
+test.beforeEach(async ({page}) => {
+  await page.goto('http://localhost:4200')
+})
+
+test.describe('Form layout page', ()=> {
+  test.beforeEach( async({page}) => {
+    await page.getByText('Forms').click();
+    await page.getByText('Form Layouts').click();
+  })
+
+  test('input fields', async ({page})=> {
+
+    const usingTheGridEmailInput = page.locator('nb-cart', { hasText: 'Using the Grid' }).getByRole("textbox", { name: 'Email'});
+    await usingTheGridEmailInput.fill('test@test.com');
+    await usingTheGridEmailInput.clear();
+    await usingTheGridEmailInput.pressSequentially('test2@test.com');
+
+    //generic assertion
+    const inputValue = await usingTheGridEmailInput.inputValue();
+    expect(inputValue).toEqual('test2@test.com');
+
+    //locator assertion
+    await expect(usingTheGridEmailInput).toHaveValue('test2@test.com');
+  })
+
+  test('radio buttons', async ({page})=> {
+    const usingTheGridForm = page.locator('nb-cart', {hasText: 'Using the Grid'});
+    //first way
+    await usingTheGridForm.getByLabel('Option 1').check({force: true});
+    //second way
+    await usingTheGridForm.getByRole('radio', {name: 'Option 1'}).check({force: true});
+    const radioStatus = await usingTheGridForm.getByRole('radio', {name: 'Option 1'}).isChecked();
+    expect(radioStatus).toBeTruthy();
+    await expect(usingTheGridForm.getByRole('radio', {name: 'Option 1'})).toBeChecked();
+
+    await usingTheGridForm.getByRole('radio', {name: 'Option 2'}).check({force: true});
+    expect(await usingTheGridForm.getByRole('radio', {name: 'Option 1'}).isChecked()).toBeFalsy();
+    expect(await usingTheGridForm.getByRole('radio', {name: 'Option 2'}).isChecked()).toBeTruthy();
+  })
+})
+
+test('checkboxes', async ({page})=> {
+  await page.getByText('Modal & Overlays').click();
+  await page.getByText('Toastr').click();
+  await page.getByRole('checkbox', {name: 'Hide on click'}).check({force: true}); //difference between click and check, if checkbox already checked then checkbox won't be deselected
+  await page.getByRole('checkbox', {name: 'Hide on click'}).uncheck({force: true}); //to uncheck
+
+  //TODO Check or uncheck all checkboxes
+  const allBoxes = page.getByRole('checkbox');
+  for (const box of await allBoxes.all()){ //all() created an array from allBoxes value
+    await box.check({force: true});
+    expect(await box.isChecked()).toBeTruthy();
+  }
+  for (const box of await allBoxes.all()){ //all() created an array from allBoxes value
+    await box.uncheck({force: true});
+    expect(await box.isChecked()).toBeFalsy();
+  }
+})
+
+test('lists and dropdown', async ({page})=> {
+  const dropDownMenu = page.locator('ngx-header nb-select');
+  await dropDownMenu.click();
+  page.getByRole('list') //when the list has ul tag
+  page.getByRole('listitem') //when the list has li tag
+
+  const optionList = page.locator('nb-option-list nb-option');
+  await expect(optionList).toHaveText(['Light', 'Dark', 'Cosmic', 'Corporate']);
+  await optionList.filter({hasText: 'Cosmic'}).click();
+  const header = page.locator('nb-layout-header');
+  await expect(header).toHaveCSS('background-color', 'rgb(50, 50, 89)');
+
+  const colors = {
+    "Light": "rgb(255, 255, 255)",
+    "Dark": "rgb(34, 43, 69)",
+    "Cosmic": "rgb(50, 50, 89)",
+    "Corporate": "rgb(255, 255, 255)"
+  }
+
+  await dropDownMenu.click();
+  for (const color in colors) {
+    await optionList.filter({hasText: color}).click();
+    await expect(header).toHaveCSS('background-color', colors[color]);
+    if(color != 'Corporate'){
+      await dropDownMenu.click();
+    }
+  }
+})
+
+test('dialog boxes', async ({page})=> {
+  await page.getByText('Tables & Data').click();
+  await page.getByText('Smart Table').click();
+
+  //to show browser dialogs
+  page.on('dialog', dialog => {
+    expect(dialog.message()).toEqual("Are you sure you want to delete?");
+    dialog.accept()
+  })
+  await page.getByRole('table').locator('tr', {hasText: 'mdo@gmail.com'}).locator('.nb-trash').click();
+  await expect(page.locator('table tr').first()).not.toHaveText('mdo@gmail.com');
+
+})
+
+test('web tables', async ({page})=> {
+  await page.getByText('Tables & Data').click();
+  await page.getByText('Smart Table').click();
+
+  //get row by any text in this row
+  const targetRow = page.getByRole('row', {name: 'twitter@outlook.com'});
+  await targetRow.locator('nb-edit').click();
+  await page.locator('input-editor').getByPlaceholder('Age').clear();
+  await page.locator('input-editor').getByPlaceholder('Age').fill('35');
+  await page.locator('.nb-checkmark').click();
+
+  //get the row based on the value in the specific column
+  await page.locator('.ng2-smart-pagination-nav').getByText('2').click();
+  const targetRowById = page.getByRole('row', {name: '11'}).filter({has: page.locator('td').nth(1).getByText('11')});
+  await targetRow.locator('nb-edit').click();
+  await page.locator('input-editor').getByPlaceholder('E-mail').clear();
+  await page.locator('input-editor').getByPlaceholder('E-mail').fill('test@test.com');
+  await page.locator('.nb-checkmark').click();
+  await expect(targetRowById.locator('td').nth(5)).not.toHaveText('test@test.com');
+
+  //test filter of the table
+  const ages = ["20", "30", "40", "200"];
+  for (const age of ages) {
+    await page.locator('input-filter').getByPlaceholder('Age').clear();
+    await page.locator('input-filter').getByPlaceholder('Age').fill(age);
+    await page.waitForTimeout(500);
+    const ageRows = page.locator('tbody tr');
+
+    //to create array
+    for (let row of await ageRows.all()) {
+      const cellValue = await row.locator('td').last().textContent();
+      if (age == "200") {
+        expect(await page.getByRole('table').textContent()).toContain("No data found");
+      } else  {
+        expect(cellValue).toEqual(age);
+      }
+    }
+
+  }
+})
+
+test('datepicker', async ({page})=> {
+  await page.getByText('Forms').click();
+  await page.getByText('Datepicker').click();
+
+  const calendarInputField = page.getByPlaceholder('Form Picker');
+  await calendarInputField.click();
+
+  let date = new Date();
+  date.setDate(date.getDate() + 1);
+  const expectedDate = date.getDate().toString()
+  const expectedMonthShort = date.toLocaleString('En-US', {month: 'short'});
+  const expectedMonthLong = date.toLocaleString('En-US', {month: 'long'});
+  const expectedYear = date.getFullYear();
+  const dateToAssert = `${expectedMonthShort} ${expectedDate}, ${expectedYear}`;
+
+  let calendarMonthYear = await page.locator('nb-calendar-view-mode').textContent();
+  const expectedMonthAndYear = ` ${expectedMonthLong} ${expectedYear}`;
+
+  while(!calendarMonthYear.includes(expectedMonthAndYear)) {
+    await page.locator('nb-calendar-pageable-navigation [data-name="chevron-right"]').click();
+    calendarMonthYear = await page.locator('nb-calendar-view-mode').textContent();
+  }
+
+  await page.locator('[class="day-cell ng-star-inserted"]').getByText(expectedDate, {exact: true}).click(); //full match
+  await expect(calendarInputField).toHaveValue(dateToAssert);
+})
+
+test('sliders', async ({page})=> {
+  //Update attribute
+  const tempGauge = page.locator('[tabtitle="Temperature"] ngx-temperature-dragger circle');
+  await tempGauge.evaluate(node => {
+    node.setAttribute('cx', '232.630')
+    node.setAttribute('cy', '232.630')
+  });
+  await tempGauge.click();
+
+  //Mouse movement
+  const tempBox = page.locator('[tabtitle="Temperature"] ngx-temperature-dragger');
+  await tempBox.scrollIntoViewIfNeeded();
+  const box = await tempBox.boundingBox(); //component that has x and y axes and starting point is top left corner with x,y (0,0)
+  const x = box.x + box.width / 2; //center of box
+  const y = box.y + box.height / 2; //center of box
+  await page.mouse.move(x, y);
+  await page.mouse.down() //simulate left click button on the mouse on the coordinates;
+  await page.mouse.move(x + 100, y); //moving our mouse to the right since we changes only horizontally, move to the left   await page.mouse.move(x - 100, y);
+  await page.mouse.move(x + 100, y+100);
+  await page.mouse.up();
+  await expect(tempBox).toContainText('30')
+})
+
+
+
+
+
+
